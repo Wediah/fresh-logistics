fd477201e9d08f08e7ec1ef6829026b512e0798e
Emmanuel  Wediah <emmanuelwediah@Emmanuels-MacBook-Pro.local>
2025-08-12 14:07:51 +0000

    wip

--- lib/features/shop/screens/home.dart
+++ lib/features/shop/screens/home.dart
@@ -15,30 +15,46 @@ class HomeSreen extends StatelessWidget {
               padding: const EdgeInsets.all(0),
               child: Stack(
                 children: [
-                  Container(
-                    width: 400,
-                    height: 400,
-                    padding: const EdgeInsets.all(0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(400),
-                      color: AppColors.primaryGreen,
-                    ),
-                  ),
-                  Container(
-                    width: 300,
-                    height: 300,
-                    padding: const EdgeInsets.all(0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(300),
-                      color: AppColors.primaryGreen,
-                    ),
-                  ),
+                  CircularContainer(),
                 ],
               )
             )
           ],
         )
       )
+    );
+  }
+}
+
+class CircularContainer extends StatelessWidget {
+  const CircularContainer({
+    super.key,
+    this.child,
+    this.width = 400,
+    this.height = 400,
+    this.radius = 400,
+    this.padding = 0,
+    this.backgroudColor = Colors.white
+  });
+
+  final double? width;
+  final double? height;
+  final double radius;
+  final double padding;
+  final Widget? child;
+  final Color? backgroudColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      padding: EdgeInsets.all(padding),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(radius),
+        color: backgroudColor,
+      ),
+      child: child,
     );
   }
 }
